9a368239ea63f6e63b01b832b90b81514101425a
xmly <lili.wei@ximalaya.com>
2022-05-05 13:14:48 +0800

    提供单纯任务完成通知接口

--- fastapi_server.py
+++ fastapi_server.py
@@ -23,6 +23,11 @@ async def item_id(task_id: int, task_status: int):
     task_manager.set_task_status(task_id, task_status)
     return {'task_id': task_id, "task_status": task_status}
 
+@app.get("/set_task_done/")
+async def item_id(task_id: str):
+    task_manager.set_task_status(int(task_id), 1)
+    return {'task_id': task_id, "task_status": 1}
+
 if __name__ == '__main__':
     # start fastapi
     uvicorn.run("fastapi_server:app", port=8000, reload=True)
